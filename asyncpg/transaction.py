e9bdf404a64878d356c52e75f35968c52ac76a41
James Robinson <james.robinson@noteable.io>
2022-07-28 13:25:54 -0400

    Log most recent N statements to try to get a handle on why we croak on the history of the connection use when we die of 'cannot use Connection.transaction() in a manually started transaction'

--- asyncpg/transaction.py
+++ asyncpg/transaction.py
@@ -6,9 +6,13 @@
 
 
 import enum
+import structlog
+
 
 from . import connresource
 from . import exceptions as apg_errors
+
+logger = structlog.get_logger(__name__)
 
 
 class TransactionState(enum.Enum):
@@ -19,11 +23,11 @@ class TransactionState(enum.Enum):
     FAILED = 4
 
 
-ISOLATION_LEVELS = {'read_committed', 'serializable', 'repeatable_read'}
+ISOLATION_LEVELS = {"read_committed", "serializable", "repeatable_read"}
 ISOLATION_LEVELS_BY_VALUE = {
-    'read committed': 'read_committed',
-    'serializable': 'serializable',
-    'repeatable read': 'repeatable_read',
+    "read committed": "read_committed",
+    "serializable": "serializable",
+    "repeatable read": "repeatable_read",
 }
 
 
@@ -35,16 +39,25 @@ class Transaction(connresource.ConnectionResource):
     function.
     """
 
-    __slots__ = ('_connection', '_isolation', '_readonly', '_deferrable',
-                 '_state', '_nested', '_id', '_managed')
+    __slots__ = (
+        "_connection",
+        "_isolation",
+        "_readonly",
+        "_deferrable",
+        "_state",
+        "_nested",
+        "_id",
+        "_managed",
+    )
 
     def __init__(self, connection, isolation, readonly, deferrable):
         super().__init__(connection)
 
         if isolation and isolation not in ISOLATION_LEVELS:
             raise ValueError(
-                'isolation is expected to be either of {}, '
-                'got {!r}'.format(ISOLATION_LEVELS, isolation))
+                "isolation is expected to be either of {}, "
+                "got {!r}".format(ISOLATION_LEVELS, isolation)
+            )
 
         self._isolation = isolation
         self._readonly = readonly
@@ -57,13 +70,14 @@ class Transaction(connresource.ConnectionResource):
     async def __aenter__(self):
         if self._managed:
             raise apg_errors.InterfaceError(
-                'cannot enter context: already in an `async with` block')
+                "cannot enter context: already in an `async with` block"
+            )
         self._managed = True
         await self.start()
 
     async def __aexit__(self, extype, ex, tb):
         try:
-            self._check_conn_validity('__aexit__')
+            self._check_conn_validity("__aexit__")
         except apg_errors.InterfaceError:
             if extype is GeneratorExit:
                 # When a PoolAcquireContext is being exited, and there
@@ -89,18 +103,26 @@ class Transaction(connresource.ConnectionResource):
     @connresource.guarded
     async def start(self):
         """Enter the transaction or savepoint block."""
-        self.__check_state_base('start')
+        self.__check_state_base("start")
         if self._state is TransactionState.STARTED:
             raise apg_errors.InterfaceError(
-                'cannot start; the transaction is already started')
+                "cannot start; the transaction is already started"
+            )
 
         con = self._connection
 
         if con._top_xact is None:
             if con._protocol.is_in_transaction():
+                logger.error(
+                    "bad transaction state for connection",
+                    connection_id=id(con),
+                    tx_state=con._protocol._protocol.xact_status,
+                    recent_statements=list(con.recent_statements),
+                )
                 raise apg_errors.InterfaceError(
-                    'cannot use Connection.transaction() in '
-                    'a manually started transaction')
+                    "cannot use Connection.transaction() in "
+                    "a manually started transaction"
+                )
             con._top_xact = self
         else:
             # Nested transaction block
@@ -108,31 +130,33 @@ class Transaction(connresource.ConnectionResource):
                 top_xact_isolation = con._top_xact._isolation
                 if top_xact_isolation is None:
                     top_xact_isolation = ISOLATION_LEVELS_BY_VALUE[
-                        await self._connection.fetchval(
-                            'SHOW transaction_isolation;')]
+                        await self._connection.fetchval("SHOW transaction_isolation;")
+                    ]
                 if self._isolation != top_xact_isolation:
                     raise apg_errors.InterfaceError(
-                        'nested transaction has a different isolation level: '
-                        'current {!r} != outer {!r}'.format(
-                            self._isolation, top_xact_isolation))
+                        "nested transaction has a different isolation level: "
+                        "current {!r} != outer {!r}".format(
+                            self._isolation, top_xact_isolation
+                        )
+                    )
             self._nested = True
 
         if self._nested:
-            self._id = con._get_unique_id('savepoint')
-            query = 'SAVEPOINT {};'.format(self._id)
+            self._id = con._get_unique_id("savepoint")
+            query = "SAVEPOINT {};".format(self._id)
         else:
-            query = 'BEGIN'
-            if self._isolation == 'read_committed':
-                query += ' ISOLATION LEVEL READ COMMITTED'
-            elif self._isolation == 'repeatable_read':
-                query += ' ISOLATION LEVEL REPEATABLE READ'
-            elif self._isolation == 'serializable':
-                query += ' ISOLATION LEVEL SERIALIZABLE'
+            query = "BEGIN"
+            if self._isolation == "read_committed":
+                query += " ISOLATION LEVEL READ COMMITTED"
+            elif self._isolation == "repeatable_read":
+                query += " ISOLATION LEVEL REPEATABLE READ"
+            elif self._isolation == "serializable":
+                query += " ISOLATION LEVEL SERIALIZABLE"
             if self._readonly:
-                query += ' READ ONLY'
+                query += " READ ONLY"
             if self._deferrable:
-                query += ' DEFERRABLE'
-            query += ';'
+                query += " DEFERRABLE"
+            query += ";"
 
         try:
             await self._connection.execute(query)
@@ -145,35 +169,35 @@ class Transaction(connresource.ConnectionResource):
     def __check_state_base(self, opname):
         if self._state is TransactionState.COMMITTED:
             raise apg_errors.InterfaceError(
-                'cannot {}; the transaction is already committed'.format(
-                    opname))
+                "cannot {}; the transaction is already committed".format(opname)
+            )
         if self._state is TransactionState.ROLLEDBACK:
             raise apg_errors.InterfaceError(
-                'cannot {}; the transaction is already rolled back'.format(
-                    opname))
+                "cannot {}; the transaction is already rolled back".format(opname)
+            )
         if self._state is TransactionState.FAILED:
             raise apg_errors.InterfaceError(
-                'cannot {}; the transaction is in error state'.format(
-                    opname))
+                "cannot {}; the transaction is in error state".format(opname)
+            )
 
     def __check_state(self, opname):
         if self._state is not TransactionState.STARTED:
             if self._state is TransactionState.NEW:
                 raise apg_errors.InterfaceError(
-                    'cannot {}; the transaction is not yet started'.format(
-                        opname))
+                    "cannot {}; the transaction is not yet started".format(opname)
+                )
             self.__check_state_base(opname)
 
     async def __commit(self):
-        self.__check_state('commit')
+        self.__check_state("commit")
 
         if self._connection._top_xact is self:
             self._connection._top_xact = None
 
         if self._nested:
-            query = 'RELEASE SAVEPOINT {};'.format(self._id)
+            query = "RELEASE SAVEPOINT {};".format(self._id)
         else:
-            query = 'COMMIT;'
+            query = "COMMIT;"
 
         try:
             await self._connection.execute(query)
@@ -184,15 +208,15 @@ class Transaction(connresource.ConnectionResource):
             self._state = TransactionState.COMMITTED
 
     async def __rollback(self):
-        self.__check_state('rollback')
+        self.__check_state("rollback")
 
         if self._connection._top_xact is self:
             self._connection._top_xact = None
 
         if self._nested:
-            query = 'ROLLBACK TO {};'.format(self._id)
+            query = "ROLLBACK TO {};".format(self._id)
         else:
-            query = 'ROLLBACK;'
+            query = "ROLLBACK;"
 
         try:
             await self._connection.execute(query)
@@ -207,7 +231,8 @@ class Transaction(connresource.ConnectionResource):
         """Exit the transaction or savepoint block and commit changes."""
         if self._managed:
             raise apg_errors.InterfaceError(
-                'cannot manually commit from within an `async with` block')
+                "cannot manually commit from within an `async with` block"
+            )
         await self.__commit()
 
     @connresource.guarded
@@ -215,24 +240,26 @@ class Transaction(connresource.ConnectionResource):
         """Exit the transaction or savepoint block and rollback changes."""
         if self._managed:
             raise apg_errors.InterfaceError(
-                'cannot manually rollback from within an `async with` block')
+                "cannot manually rollback from within an `async with` block"
+            )
         await self.__rollback()
 
     def __repr__(self):
         attrs = []
-        attrs.append('state:{}'.format(self._state.name.lower()))
+        attrs.append("state:{}".format(self._state.name.lower()))
 
         if self._isolation is not None:
             attrs.append(self._isolation)
         if self._readonly:
-            attrs.append('readonly')
+            attrs.append("readonly")
         if self._deferrable:
-            attrs.append('deferrable')
+            attrs.append("deferrable")
 
-        if self.__class__.__module__.startswith('asyncpg.'):
-            mod = 'asyncpg'
+        if self.__class__.__module__.startswith("asyncpg."):
+            mod = "asyncpg"
         else:
             mod = self.__class__.__module__
 
-        return '<{}.{} {} {:#x}>'.format(
-            mod, self.__class__.__name__, ' '.join(attrs), id(self))
+        return "<{}.{} {} {:#x}>".format(
+            mod, self.__class__.__name__, " ".join(attrs), id(self)
+        )
